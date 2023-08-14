6b2a100d3b1d85fd452368590c83d59792fcda39
ikorolevadev <i.koroleva@make.com>
2023-08-14 09:54:36 +0200

    initial project, reading yaml from file

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+// console.log(obj);
+
+function App() {
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
--- server/main.ts
+++ server/main.ts
@@ -1,6 +1,48 @@
-import express, { NextFunction, Request, Response } from "express";
-//import swaggerUi from 'swagger-ui-express';
+import dotenv from "dotenv";
+import express, { Express, Request, Response } from "express";
+import path from "path";
+import cors from "cors";
+import http from "http";
+const yaml = require('js-yaml');
+const fs = require('fs');
 
-const instance = express() 
+dotenv.config();
 
-instance.use(express.json())
+const instance: Express = express();
+
+instance.use(express.json());
+instance.use(cors());
+
+instance.get('/', (req: Request, res: Response) => {
+  res.send('Hello World From the Typescript Server!')
+});
+
+const port = process.env.PORT || 8000;
+
+instance.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+});
+
+
+try {
+  const doc = yaml.load(fs.readFileSync('src/data/openapi.yaml', 'utf8'));
+  // console.log(doc);
+  const jsonData = JSON.stringify(doc);
+  console.log(jsonData);
+  fs.writeFileSync('src/data/input_file.json', jsonData, 'utf8');
+} catch (e) {
+  console.log(e);
+}
+
+// Read the Yaml file
+//const data = fs.readFileSync('src/data/openapi.yaml', 'utf8');
+
+//Convert Yml object to JSON
+//const yamlData = yaml.load(data);
+
+//Write JSON to Yml
+//const jsonData = JSON.stringify(yamlData);
+// fs.writeFileSync('src/data/input_file.json', jsonData, 'utf8');
+
+
+
